bb275c8f5d6925007f55cb68754396e051674b6d
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2024-01-22 16:36:15 +1100

    finish booking function

--- frontend/src/reducers/bookingReducer.js
+++ frontend/src/reducers/bookingReducer.js
@@ -1,13 +1,24 @@
 import { createEntityAdapter, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { server } from "../config/serverURL";
 
 const bookingAdapter=createEntityAdapter();
 const initialState=bookingAdapter.getInitialState();
 
+export const addBooking=createAsyncThunk('booking/addBooking', async({data, token, price})=>{
+    const config={
+        headers:{
+            authorization: `Bearer ${token}`
+        }
+    }
+    data.price=price;
+    const response=await axios.post(server+"booking", data, config);
+    return response.data;
+})
 const bookingSlice=createSlice({
     name:'booking',
     initialState, 
-    reducers:{
-
-    }
+    reducers:{}
 })
+
 export default bookingSlice.reducer;
